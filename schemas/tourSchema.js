97541d5397b4df0ef694699a101eae2418e0b6a9
dmjendor@gmail.com <dmjendor@gmail.com>
2026-02-11 11:15:25 -0500

    feat: implement tour schema and establish MongoDB connection; update environment configuration

---
+++ schemas/tourSchema.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+    trim: true,
+  },
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty'],
+    enum: ['easy', 'medium', 'difficult'],
+  },
+  ratingsAverage: {
+    type: Number,
+    default: 4.5,
+    min: 1,
+    max: 5,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true,
+  },
+  description: String,
+  imageCover: String,
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('Tour', tourSchema);
--- server.js
+++ server.js
@@ -1,8 +1,35 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const app = require('./app');
+const Tour = require('./schemas/tourSchema');
 
 dotenv.config({ path: './config.env' });
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.PASSWORD);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB Connection Successful'));
 
-const app = require('./app');
+const testTour = new Tour({
+  name: 'Testing Tour 2',
+  duration: 5,
+  maxGroupSize: 25,
+  difficulty: 'easy',
+  price: 501,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log('ERROR: ', err);
+  });
 
 app.listen(process.env.PORT, () => {
   console.log(`App running on port ${process.env.PORT}...`);
