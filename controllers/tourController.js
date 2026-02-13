9b1c87cfb4cbbdd381334e56047b669e4685f1aa
dmjendor@gmail.com <dmjendor@gmail.com>
2026-02-13 11:47:30 -0500

    feat: implement advanced filtering, sorting, and pagination for tours; add data import/export functionality

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -6,7 +6,53 @@ const Tour = require('../models/tourModel');
  */
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    // BUILD QUERY
+    // 1a)  Filtering
+    const { page, limit, sort, fields, ...queryObj } = req.query;
+
+    // 1b) Advanced Filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+
+    // 2) Build Query
+    let query = Tour.find(JSON.parse(queryStr));
+    console.log(req.query);
+
+    // 3) Sorting
+    if (sort) {
+      const sortBy = sort.split(',').join(' ');
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-name');
+    }
+
+    // 4) Field Limiting
+    if (fields) {
+      const queryFields = fields.split(',').join(' ');
+      query = query.select(queryFields);
+    } else {
+      query = query.select('-__v');
+    }
+
+    // 5) Pagination
+    const pageNum = Number(page) || 1;
+    const limitNum = Number(limit) || 100;
+    const skip = (pageNum - 1) * limitNum;
+    query = query.skip(skip).limit(limitNum);
+
+    if (page) {
+      const numTours = await Tour.countDocuments();
+      if (skip > numTours) throw new Error('This page does not exist.');
+    }
+
+    // EXECUTE QUERY
+    const tours = await query;
+
+    // const tours = await Tour.find()
+    //   .where('duration')
+    //   .lte(5)
+    //   .where('difficulty')
+    //   .equals('easy');
     res.status(200).json({
       status: 'success',
       requestedAt: req.requestTime,
---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const fs = require('fs');
+const Tour = require('../../models/tourModel');
+
+dotenv.config({ path: './config.env' });
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.PASSWORD);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB Connection Successful'));
+
+// READ JSON FILE
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
+);
+
+//IMPORT DATA INTO DB
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('Data successfully loaded');
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// DELETE ALL DATA FROM COLLECTION
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('Data successfuly deleted.');
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+console.log(process.argv);
