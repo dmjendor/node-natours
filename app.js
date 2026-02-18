b02a9e2657135ce17b19ca7b5e1718b1d93a050f
dmjendor@gmail.com <dmjendor@gmail.com>
2026-02-18 07:56:38 -0500

    feat: implement custom error handling with AppError class and global error handler

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const morgan = require('morgan');
+const AppError = require('./utils/appError');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const globalErrorHandler = require('./controllers/errorController');
 
 const app = express();
 
@@ -18,11 +20,13 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
-app.all('*', function (req, res, next) {
-  res.status(404).json({
-    status: 'fail',
-    message: `Can't find ${req.originalUrl} on this server.`,
-  });
-  next();
+app.all('*', (req, res, next) => {
+  // const err = new Error(`Can't find ${req.originalUrl} on this server.`);
+  // err.status = 'fail';
+  // err.statusCode = 400;
+
+  next(new AppError(`Can't find ${req.originalUrl} on this server.`, 404));
 });
+
+app.use(globalErrorHandler);
 module.exports = app;
